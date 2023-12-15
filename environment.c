e41b27dec06c2e09e39789ffb52e9cc5990a2b1b
root <kbthaba@gmail.com>
2023-12-15 12:51:22 -0800

    my environment file

---
+++ environment.c
@@ -0,0 +1,50 @@
+#include "shell.h"
+
+/**
+ * cse - creates shell environment
+ * @ev: Environment passed to the main
+ *
+ * Return: pointer to new environment
+ */
+char **cse(char **ev)
+{
+    char **n = NULL;
+    size_t x;
+
+    for (x = 0; ev[x] != NULL; x++)
+        ;
+
+    n = malloc(sizeof(char *) * (x + 1));
+
+    if (n == NULL)
+    {
+        perror("Fatal Error");
+        ex(1);
+    }
+
+    for (x = 0; ev[x] != NULL; x++)
+        n[x] = dcs(ev[x]);
+
+    n[x] = NULL;
+
+    return (n);
+}
+
+/**
+ * dse - free shell
+ * @ev: Shell environment
+ */
+void dse(char **ev)
+{
+    unsigned int x;
+
+    x = 0;
+
+    while (ev[x] != NULL)
+    {
+        free(ev[x]);
+        x++;
+    }
+
+    free(ev);
+}
