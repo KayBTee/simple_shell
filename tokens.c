470f7b521fff7327173146d259ec68d6a5df22d2
root <kbthaba@gmail.com>
2023-12-15 13:31:30 -0800

    my tokens file

---
+++ tokens.c
@@ -0,0 +1,43 @@
+#include "shell.h"
+
+/**
+ * tokenize_command - tokenizes a command buffer with a delimeter
+ * @cmd_buffer: command buffer to tokenize
+ * @delimeter: delimeter to tokenize along
+ *
+ * Return: pointer to an array of pointers to the tokens
+ */
+char **tokenize_command(char *cmd_buffer, char *delimeter)
+{
+    char **tokens = NULL;
+    size_t x = 0, mcount = 10;
+
+    if (cmd_buffer == NULL)
+        return NULL;
+
+    tokens = malloc(sizeof(char *) * mcount);
+
+    if (tokens == NULL)
+    {
+        perror("Fatal Error");
+        return NULL;
+    }
+
+    while ((tokens[x] = custom_strtok_r(cmd_buffer, delimeter)) != NULL)
+    {
+        x++;
+        if (x == mcount)
+        {
+            tokens = realloc_custom_pointer(tokens, &mcount);
+
+            if (tokens == NULL)
+            {
+                perror("Fatal Error");
+                return NULL;
+            }
+        }
+        cmd_buffer = NULL;
+    }
+
+    return tokens;
+}
